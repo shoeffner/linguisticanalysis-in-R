46284dfde0d0b86eceacc82c3a09dafb9e48802f
Sebastian Höffner <info@sebastian-hoeffner.de>
2017-02-11 15:01:47 +0100

    two-sided test

--- hw6-group-g1.R
+++ hw6-group-g1.R
@@ -6,15 +6,15 @@ library('lme4')
 # We assume alpha = 0.05 for all tests.
 
 ttest <- function (model, effect, df, alpha=0.05) {
-    # Performs a one-sided t-test for a given alpha value on the given model.
+    # Performs a two-sided t-test for a given alpha value on the given model.
     t = abs(coef(summary(model))[effect, 't value'])
-    q = qt(1 - alpha, df=df)
+    q = qt(1 - alpha / 2, df=df)
     paste('|t| =', round(t, 3), '>', round(q, 3), '?', t > q)
 }
 
 
 # 1. Are reaction times affected by the number of trials?
-# 2 DOF, one-sided test: Pr(|t| < 2.92) = .95
+# 2 DOF, two-sided test: Pr(|t| < 4.303) = .95
 # No: |t| = 2.435
 
 model1 = lm(RT ~ Trial, lexdec)
@@ -23,17 +23,17 @@ print(paste("Is reaction time affected by the number of trials only?", ttest(mod
 
 # 2. Are reaction times affected by the number of trials,
 # taking Subject and Word differences (their varying means) into account?
-# 5 DOF, one-sided test: Pr(|t| < 2.015) = .95
-# Yes: |t| = 2.647 > 2.015
+# 5 DOF, two-sided test: Pr(|t| < 2.571) = .95
+# Yes: |t| = 2.647 > 2.571
 
 model2 = lmer(RT ~ Trial + (1|Subject) + (1|Word), lexdec)
 print(paste("Is reaction time affected by the number of trials?", ttest(model2, 'Trial', 5)))
 
 
 # 3. Are reaction times also affected by the native language?
-# 6 DOF, one-sided test: Pr(|t| < 1.943) = .95
-# Yes (native language): |t| = 2.573 > 1.943
-# Yes (trials): |t| = 2.647 > 1.943
+# 6 DOF, two-sided test: Pr(|t| < 2.447) = .95
+# Yes (native language): |t| = 2.573 > 2.447
+# Yes (trials): |t| = 2.647 > 2.447
 
 model3 = lmer(RT ~ Trial + (1|Subject) + (1|Word) + NativeLanguage, lexdec)
 print(paste("Is reaction time affected by native language?", ttest(model3, 'NativeLanguageOther', 6)))
@@ -41,11 +41,11 @@ print(paste("Is reaction time still affected by trials if considering native lan
 
 
 # 4. Subjects are affected differently depending on the number of trials.
-# 7 DOF, one-sided test: Pr(|t| < 1.895) = .95
+# 7 DOF, two-sided test: Pr(|t| < 2.365) = .95
 # !! The model does not converge. As such the t-values can not be used for predictions,
 #    but if we could, this would be the results:
-# No (trials): |t| = 1.369 > 1.415
-# Yes (native language): |t| = 2.913 > 1.415
+# No (trials): |t| = 1.369 > 2.365
+# Yes (native language): |t| = 2.913 > 2.365
 
 model4 = lmer(RT ~ Trial + (1 + Trial|Subject) + (1|Word) + NativeLanguage, lexdec)
 print(paste("Is reaction time still affected by trials if considering native language and by-subject variance?", ttest(model4, 'Trial', 7)))
